27249b1716ff07d31175a00fd885cf6bb08876e4
Warren McDonald <warrenjmac@mail.com>
2019-10-29 18:45:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 class Dog 
   def name = (dog_name)
-    this_dogs_name = dog_name 
+    @this_dogs_name = dog_name 
     def name
-      this_dogs_name
+      @this_dogs_name
     end 
 end 
 lassie = Dog.new 
